88c4724ef11844a24b8103c15bf9dcc6bf11899e
Keenan Satriatama <cvnvnasdf24@gmail.com>
2026-04-08 15:56:14 +0700

    feat: add registration feature

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,5 +4,6 @@ export const axiosInstance = axios.create({
   baseURL: "https://securepies-us.backendless.app/api",
 });
 
-// bikir84466@iaciu.com
-// Admin123
+export const axiosInstance2 = axios.create({
+  baseURL: "http://localhost:8001/",
+})
